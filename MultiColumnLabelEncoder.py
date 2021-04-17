c3f9e168af223e1c5b23efdc8681eadc3eb43af7
Kowsher <ga.kowsher@gmail.com>
2021-04-17 13:02:41 +0600

    Create MultiColumnLabelEncoder.py

---
+++ MultiColumnLabelEncoder.py
@@ -0,0 +1,33 @@
+from sklearn import preprocessing
+le = preprocessing.LabelEncoder()
+class MultiColumnLabelEncoder:
+    def __init__(self,columns = None):
+        self.columns = columns # array of column names to encode
+
+    def fit(self,X,y=None):
+        return self # not relevant here
+
+    def transform(self,X):
+        '''
+        Transforms columns of X specified in self.columns using
+        LabelEncoder(). If no columns specified, transforms all
+        columns in X.
+        '''
+        output = X.copy()
+        if self.columns is not None:
+            for col in self.columns:
+                output[col] = le.fit_transform(output[col])
+        else:
+            for colname,col in output.iteritems():
+                output[colname] = le.fit_transform(col)
+        return output
+
+    def fit_transform(self,X,y=None):
+        return self.fit(X,y).transform(X)
+
+subset = []
+for col in X.columns:
+    if X[col].dtype.name == 'object':
+        subset.append(col)
+
+XX=MultiColumnLabelEncoder(columns = subset).fit_transform(X)
